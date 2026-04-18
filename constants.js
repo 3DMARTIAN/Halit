62a753080debfa3e800585837353d2ac64217479
begeritos <72303665+3DMARTIAN@users.noreply.github.com>
2026-04-18 10:41:03 +0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,12 @@
+// Fundamental constants inspired by halit
+
+export const HALIT_COLOR = "#F8F8FF";
+export const PURITY_LEVEL = 0.99;
+export const MINIMALISM = true;
+
+export const PRINCIPLES = [
+  "simplicity",
+  "clarity",
+  "structure",
+  "balance"
+];
